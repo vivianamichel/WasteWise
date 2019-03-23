aa14c2ef0d8e4fc615d0a84e053e1874337a78c6
Viviana Michel <vivianamichel@Vivianas-MacBook-Pro.local>
2019-03-23 19:02:36 -0400

    python code

--- Main.py
+++ Main.py
@@ -1,61 +1,32 @@
+userData = {}
+userPassword = {}
+class User:
+    def __init__(self, name, email, password, d, pdict):
+        self.name = name
+        self.email = email
+        self.score = 0
+        self.password = password
+        self.data = d
+        self.passwords = pdict
+        self.data.update({name: self.score})
+        self.passwords.update({email: password})
 
-import jinja2
-import webapp2
-import json
-from google.appengine.ext import ndb
-from google.appengine.api import urlfetch
-from google.appengine.api import users
+    def add_score(self, score):
+        self.score += score
+        self.data[self.name] = self.score
 
-
-jinja_env = jinja2.Environment(
-        loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
-        extensions=['jinja2.ext.autoescape'],
-        autoescape=True)
-
-def get_key(me):
-    return ndb.Key("Visitor", me.user_id())
-
-def get_visitor():
-    me = users.get_current_user()
-    if not me:
-        return None
-    my_key = get_key(me)
-    return my_key.get()
-
-class Visitor(ndb.Model):
-    id = ndb.StringProperty(required=False)
-    fullname = ndb.StringProperty(required=False)
-    givenname = ndb.StringProperty(required=False)
-    imageurl = ndb.StringProperty(required=False)
-    email = ndb.StringProperty(required=False)
-
-class InfoPage(webapp2.RequestHandler):
-    def get(self):
-       user = users.get_current_user()
-       if not user: #User is not signed in
-           info = jinja_env.get_template('templates/info.html')
-           jinja_values = {
-               'log_url': users.create_login_url('/')
-               }
-            self.response.write(info.render(jinja_values))
+def login(self, email, password):
+    if email in userPassword:
+        if self.passwords[self.email] == password:
+            print("youre in")
         else:
-            my_visitor = get_visitor()
-            if not my_visitor:
-                my_visitor = Visitor(
-                    id = user.user_id(),
-                    name = user.nickname(),
-                    email = user.email())
+            print("incorrect password")
+    else:
+        print("Email not associated with user")
+def scoreboard(dict):
+        for name in sorted(dict, key = dict.get, reverse=True):
+            print(name, dict[name])
 
-                    my_visitor.put()
-            userhome = jinja_env.get_template('templates/home.html')
-
-            jinja_values = {
-                    'name': user.nickname(),
-                    'email_addr': user.email(),
-                    'user_id': user.user_id(),
-                    'log_url': users.create_logout('/'),
-            }
-
-app = webapp2.WSGIApplication([
-    ('/', InfoPage),
-])
+viviana = User("viviana", "@@@", "123", userData, userPassword)
+print(userPassword)
+login(viviana, "@@@", "12")
